4e8c72bdaba20106712cb872f40e772a53e0569c
Nagalim <nagalim@protonmail.com>
2023-06-28 21:26:46 -0400

    Update PeercoinSim.py
    Updated from MatthewLM

--- PeercoinSim.py
+++ PeercoinSim.py
@@ -3,57 +3,72 @@ import matplotlib.pyplot as plt
 
 dayyear=(365*33+8)/33
 secday=60*60*24
-MaxSimDays=720
-#randomseed=9368
 NumSim=1000
 
 #Simulation Variables
-diff=20
+diff=20.43
+StaticReward=1.34
+MaxSimDays=365*2
+geometric=True
+
+# Precomputed probability for 31-90 days to be adjusted by value/diff
+probsecs = [2**224 * (x+1) / (2**256) for x in range(60)]
 
 #Model
 def RandomDaysToMint(outValue,difficulty,rng):
-    rnd = 1
-    DaysToMint=0
-    for x in range(MaxSimDays):
+
+    adj = outValue / difficulty
+    probs = [1 - (1 - probsecs[x]*adj)**secday for x in range(60)]
+
+    DaysToMint=31
+    for x in range(60):
+
         rnd = rng.random()
-        #print("random")
-        #print(rnd)
-        probday=outValue*secday*min(60,max((x-30),0))/(diff*2**32)
+        probday = probs[x]
+
         if rnd<probday: break
+
         DaysToMint+=1
-    else: DaysToMint=10**11
-    #print("DaysToMint")
-    #print(MatDaysToMint)
+
+    else: DaysToMint=DaysToMint+rng.geometric(probs[59])
+
     return DaysToMint
 
+rng = np.random.default_rng()
+
 #Reward Wrapper
-def MintRewards(outValue,difficulty):
-    avgreward=0
-    for y in range(NumSim):
-        #rewardseed=randomseed+y
-        rng = np.random.default_rng()
+def MintRewards(outValue, difficulty):
+
+    totalreward = 1 if geometric else 0
+    totaldays = 0
+
+    for _ in range(NumSim):
+
         MintDays=RandomDaysToMint(outValue,difficulty,rng)
+
         #Coinage Limit
-        CoinageReward=0.03*outValue*min(365,(MintDays))/dayyear
-        StaticReward=1.3
-        normreward=(CoinageReward+StaticReward)/MintDays
-        #print("reward/day")
-        #print(normreward)
-        avgreward+=normreward/NumSim
-    return avgreward
+        if MintDays < MaxSimDays:
+            reward=0.03*outValue*min(365, MintDays)/dayyear + StaticReward
+            if geometric:
+                totalreward *= 1+(reward/outValue)
+            else:
+                totalreward+=reward
+
+        # Add to total days the amount of time waited on this mint upto the
+        # maximum wait time
+        totaldays += min(MintDays, MaxSimDays)
+
+    # Return annualised percentage
+    if geometric:
+        return (totalreward**(dayyear/totaldays) - 1)*100
+    rewardperday = totalreward/totaldays
+    return rewardperday/outValue*36500
+
+OutArray=[2**(x/4) for x in range(50)]
+print(OutArray)
 
 def OutputWrapper():
-    #OutArray=[0.01,0.1,1,10,100,1000]
-    #OutArray=[10,20,30,35,40,50,75,100]
-    OutArray=[1,2,5,8,10,15,20,30,50,75,100,150,200,300,500,750,1000]
-    RewardArray=[]
-    for z in OutArray:
-        probdaymat=z*secday*(60)/(diff*2**32)
-        thisreward=MintRewards(z,diff)
-        RewardArray.append(thisreward/z)
-        #print("Output: {}".format(z))
-        #print("probability: {}%/day".format(probdaymat*100))
-        #print("Reward/Coin/Day: {}".format(thisreward/z))
+    RewardArray=[MintRewards(x, diff) for x in OutArray]
     PlotArray=[OutArray,RewardArray]
     return PlotArray
 
@@ -63,8 +78,8 @@ for w in range(10):
     ax.scatter(RandPlot[0],RandPlot[1])
     print(w)
 ax.set_xlabel("UTXO Size")
-ax.set_ylabel("Reward/Coin/Day")
+ax.set_ylabel("% Reward / Yr")
 plt.xscale("log")
-plt.yscale("log")
+plt.grid()
 plt.show()
 
