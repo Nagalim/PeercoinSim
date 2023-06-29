a93f76c13f5b1fcd6d66f9fb883a6fb6b271df7c
Nagalim <nagalim@protonmail.com>
2023-06-28 21:27:33 -0400

    Update PeercoinSim.py
    Added Diff and Static arrays

--- PeercoinSim.py
+++ PeercoinSim.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 
 dayyear=(365*33+8)/33
 secday=60*60*24
-NumSim=1000
+NumSim=100
 
 #Simulation Variables
-diff=20.43
-StaticReward=1.34
-MaxSimDays=365*2
+#StaticReward=1.34
+MaxSimDays=365*8
 geometric=True
 
 # Precomputed probability for 31-90 days to be adjusted by value/diff
@@ -37,7 +36,7 @@ def RandomDaysToMint(outValue,difficulty,rng):
 rng = np.random.default_rng()
 
 #Reward Wrapper
-def MintRewards(outValue, difficulty):
+def MintRewards(outValue, difficulty, static):
 
     totalreward = 1 if geometric else 0
     totaldays = 0
@@ -48,7 +47,7 @@ def MintRewards(outValue, difficulty):
 
         #Coinage Limit
         if MintDays < MaxSimDays:
-            reward=0.03*outValue*min(365, MintDays)/dayyear + StaticReward
+            reward=0.03*outValue*min(dayyear, MintDays)/dayyear + static
             if geometric:
                 totalreward *= 1+(reward/outValue)
             else:
@@ -65,21 +64,40 @@ def MintRewards(outValue, difficulty):
     return rewardperday/outValue*36500
 
 OutArray=[2**(x/4) for x in range(50)]
+#OutArray=[10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
 print(OutArray)
 
-def OutputWrapper():
-    RewardArray=[MintRewards(x, diff) for x in OutArray]
-    PlotArray=[OutArray,RewardArray]
-    return PlotArray
+def OutputWrapper(diff,static):
+    RewardArray=[MintRewards(x,diff,static) for x in OutArray]
+    #PlotArray=[OutArray,RewardArray]
+    return RewardArray
+
+def DifficultyWrapper(diff,static):
+    NumCurves=20
+    optimal=0
+    for w in range(NumCurves):
+        RewardArray=OutputWrapper(diff,static)
+        #ax.scatter(RandPlot[0],RandPlot[1])
+        #print(w)
+        optimal+=OutArray[RewardArray.index(max(RewardArray))]/NumCurves
+    print(diff)
+    print(static)
+    print(optimal)
+    return optimal
 
 fig, ax = plt.subplots()
-for w in range(10):
-    RandPlot=OutputWrapper()
-    ax.scatter(RandPlot[0],RandPlot[1])
-    print(w)
-ax.set_xlabel("UTXO Size")
-ax.set_ylabel("% Reward / Yr")
-plt.xscale("log")
+DiffArray=[8,12,16,20,24,28,32]
+#DiffArray=[10,20,30]
+#StaticArray=[0,0.5,1,1.34,1.5,2,3,4,5,10,20,100]
+StaticArray=[0.5,1,1.34,2,3]
+for u in StaticArray:
+    OptimalArray=[DifficultyWrapper(v,u) for v in DiffArray]
+    ax.scatter(DiffArray,OptimalArray)
+ax.set_xlabel("Difficulty (unitless)")
+ax.set_ylabel("Optimal Output (PPC)")
+ax.legend(StaticArray,title="Static Reward (PPC)")
+#plt.xscale("log")
 plt.grid()
 plt.show()
+
 
